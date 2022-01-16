5b09be8f05a3375b083742ecb93fecea706725e7
matt <mdeloux@gmail.com>
2022-01-16 01:56:02 -0600

    commit tree daee614651ab43dcc718c624febea2240cea3911

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+
+function superbowlWin(object) {
+    for (const record of object) {
+        if (record.result === 'W') {
+            return (record.year);
+        }
+    }
+}
+
+superbowlWin.find(year);
+
